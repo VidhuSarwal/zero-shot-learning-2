f37e29d3aec1d2dca45555f11e2eede160793975
Vidhu <61115425+VidhuSarwal@users.noreply.github.com>
2025-11-28 03:23:32 +0530

    dfs

--- src/feature_extractor.py
+++ src/feature_extractor.py
@@ -5,34 +5,35 @@
 # Contact: cetin.samet@outlook.com
 #
 
-import keras
-from keras import backend as K
-from keras.preprocessing import image
-from keras.models import Model
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 import tensorflow as tf
-from tensorflow.python.tools import freeze_graph, optimize_for_inference_lib
-
+from tensorflow import keras
+from tensorflow.keras import backend as K
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.models import Model
 import numpy as np
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-
 
 
 def get_model():
-    vgg_model = keras.applications.VGG16(include_top=True, weights='imagenet')
-    vgg_model.layers.pop()
-    vgg_model.layers.pop()
-
+    """Load VGG16 model and remove last two layers for feature extraction"""
+    vgg_model = tf.keras.applications.VGG16(include_top=True, weights='imagenet')
+    
+    # Remove last two layers (softmax classification layers)
+    # This keeps the 4096-dimensional feature vector
+    layers = [layer for layer in vgg_model.layers[:-2]]
+    
     inp = vgg_model.input
-    out = vgg_model.layers[-1].output
-
+    out = vgg_model.layers[-3].output  # Get output from fc2 layer (4096 features)
+    
     model = Model(inp, out)
     return model
 
 def get_features(model, cropped_image):
+    """Extract features from an image using the VGG16 model"""
     x = image.img_to_array(cropped_image)
     x = np.expand_dims(x, axis=0)
-    x = keras.applications.vgg16.preprocess_input(x)
-    features = model.predict(x)
+    x = tf.keras.applications.vgg16.preprocess_input(x)
+    features = model.predict(x, verbose=0)
     return features
